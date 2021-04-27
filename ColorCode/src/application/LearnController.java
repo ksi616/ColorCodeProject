769a114b4bd45cc933aecd22f2b7d06d24603bb2
ksi616 <ksi616@my.utsa.edu>
2021-04-26 20:46:24 -0500

    Created Learn Controller and Learn fx, starting with primary colors (RGB) first as to test the switch statement and function to create the colors to see if it works.

--- ColorCode/src/application/LearnController.java
+++ ColorCode/src/application/LearnController.java
@@ -8,12 +8,55 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.stage.Stage;
 
 public class LearnController {
 	
     @FXML
     private Button home;
+    
+    @FXML
+    private CheckBox red;
+
+    @FXML
+    private CheckBox blue;
+
+    @FXML
+    private CheckBox yellow;
+    
+    @FXML
+    private CheckBox orange;
+
+    @FXML
+    private CheckBox green;
+
+    @FXML
+    private CheckBox purple;
+    
+    @FXML
+    private Button add;
+    
+    int pattern = (red.isSelected() ? 0b0001 : 0)
+    			| (blue.isSelected() ? 0b0010 : 0)
+    			| (yellow.isSelected() ? 0b0100 : 0);
+    @FXML
+    void createColor(ActionEvent event) {
+    	switch (pattern) {
+    	case 0b0011:
+    		
+    		break;
+    		
+    	case 0b0101:
+    		
+    		break;
+    		
+    	case 0b0110:
+    		
+    		break;
+    	}
+    	
+    }
 
     @FXML
     void onHome(ActionEvent event) throws IOException {
